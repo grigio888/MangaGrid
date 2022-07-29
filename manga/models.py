dd83d307401e4ac31dfe748c55e0e2beeaeffa7e
grigio888 <vinicius_grigio@hotmail.com>
2022-07-29 12:33:59 +0100

    Initial Structure of DB

--- manga/models.py
+++ manga/models.py
@@ -17,30 +17,116 @@ class Sources(db.Model):
     __tablename__ = 'sources'
 
     id = db.Column(db.Integer, primary_key=True)
-    source = db.Column(db.String(100), unique=True, nullable=False)
-    source_slug = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(100), unique=True, nullable=False)
+    slug = db.Column(db.String(100), nullable=False)
     language = db.Column(db.String(100), nullable=False)
     url = db.Column(db.String(100), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.datetime.now(), onupdate=datetime.datetime.now())
 
-    def __init__(self, source, source_slug, language, url):
-        self.source = source
-        self.source_slug = source_slug
+    def __init__(self, title, slug, language, url):
+        self.title = title
+        self.slug = slug
         self.language = language
         self.url = url
 
     def __repr__(self):
-        return f'<Source {self.name}>'
+        return f'<Source {self.slug}>'
+
+    def serialize(self):
+        return {
+            'source_id': self.id,
+            'source_title': self.title,
+            'source_slug': self.slug,
+            'source_language': self.language,
+            'source_url': self.url,
+            'source_created_at': self.created_at,
+            'source_updated_at': self.updated_at
+        }
+
+
+
+
+
+# -------- STATUS -------- #
+class Status(db.Model):
+    __tablename__ = 'status'
+
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.datetime.now(), onupdate=datetime.datetime.now())
+
+    def __init__(self, status):
+        self.status = status
+
+    def __repr__(self):
+        return f'<Status {self.status}>'
+
+    def serialize(self):
+        return {
+            'status_status' : self.status
+        }
+
+
+
+
+
+# ------- AUTHORS -------- #
+class Authors(db.Model):
+    __tablename__ = 'authors'
+
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.datetime.now(), onupdate=datetime.datetime.now())
+
+    def __init__(self, author):
+        self.author = author
+
+    def __repr__(self):
+        return f'<Author {self.author}>'
+
+    def serialize(self):
+        return {
+            'authors_author' : self.author
+        }
+
+
+
+
+
+# ------- GENRES -------- #s
+class Genres(db.Model):
+    __tablename__ = 'genres'
+
+    id = db.Column(db.Integer, primary_key=True)
+    genre = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.datetime.now(), onupdate=datetime.datetime.now())
+
+    def __init__(self, genre):
+        self.genre = genre
+
+    def __repr__(self):
+        return f'<Genre {self.genre}>'
+
+    def serialize(self):
+        return {
+            'genres_genre' : self.genre
+        }
+
+
 
 
 
 # ------- MANGAS -------- #
-mangas_authors = db.Table('mangas_authors',
+mangas_fk_author = db.Table('mangas_fk_author',
     db.Column('manga_id', db.Integer, db.ForeignKey('mangas.id'), primary_key=True),
     db.Column('author_id', db.Integer, db.ForeignKey('authors.id'), primary_key=True)
 )
 
-mangas_genres = db.Table('mangas_genres',
+mangas_fk_genre = db.Table('mangas_fk_genre',
     db.Column('manga_id', db.Integer, db.ForeignKey('mangas.id')),
     db.Column('genre_id', db.Integer, db.ForeignKey('genres.id'))
 )
@@ -52,25 +138,22 @@ class Mangas(db.Model):
     title = db.Column(db.String(100), nullable=False)
     slug = db.Column(db.String(1000), nullable=False)
     image = db.Column(db.String(400), nullable=False)
-    author = db.relationship('Authors', secondary=mangas_authors, backref='mangas')
-    status = db.Column(db.String(100), default=True)
-    genre = db.relationship('Genres', secondary=mangas_genres, backref='mangas')
-    updated = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    status = db.Column(db.Integer, db.ForeignKey('status.id'), nullable=False)
     views = db.Column(db.Integer, default=0)
-    description = db.Column(db.String(2000), nullable=False)
+    description = db.Column(db.String(4000), nullable=False)
     source = db.Column(db.Integer, db.ForeignKey('sources.id'), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
-    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
-    favorites = db.relationship('Favorites', backref='mangas', lazy='dynamic')
-    ratings = db.relationship('Ratings', backref='mangas', lazy='dynamic')
-    history = db.relationship('History', backref='mangas', lazy='dynamic')
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.datetime.now(), onupdate=datetime.datetime.now())
 
-    def __init__(self, title, slug, image, status, updated, views, description, source):
+    # -- relationships -- #
+    author = db.relationship('Authors', secondary=mangas_fk_author, backref='mangas')
+    genre = db.relationship('Genres', secondary=mangas_fk_genre, backref='mangas')
+
+    def __init__(self, title, slug, image, status, views, description, source):
         self.title = title
         self.slug = slug
         self.image = image
         self.status = status
-        self.updated = updated
         self.views = views
         self.description = description
         self.source = source
@@ -80,92 +163,54 @@ class Mangas(db.Model):
 
     def serialize(self):
         return {
-            'title' : self.title,
-            'slug' : self.slug,
-            'image' : self.image,
-            'status' : self.status,
-            'updated' : self.updated,
-            'views' : self.views,
-            'description' : self.description,
-            'source' : self.source
+            'manga_id': self.id,
+            'manga_title': self.title,
+            'manga_slug': self.slug,
+            'manga_image': self.image,
+            'manga_status': self.status,
+            'manga_views': self.views,
+            'manga_description': self.description,
+            'manga_source': self.source,
+            'manga_created_at': self.created_at,
+            'manga_updated_at': self.updated_at
         }
 
 
-
-# ------- AUTHORS -------- #
-class Authors(db.Model):
-    __tablename__ = 'authors'
-
-    id = db.Column(db.Integer, primary_key=True)
-    author = db.Column(db.String(100), nullable=False)
-
-    def __init__(self, author):
-        self.author = author
-
-    def __repr__(self):
-        return f'<Author {self.author}>'
-
-    def serialize(self):
-        return {
-            'author' : self.author
-        }
-
-
-
-# ------- GENRES -------- #s
-class Genres(db.Model):
-    __tablename__ = 'genres'
-
-    id = db.Column(db.Integer, primary_key=True)
-    genre = db.Column(db.String(100), nullable=False)
-
-    def __init__(self, genre):
-        self.genre = genre
-
-    def __repr__(self):
-        return f'<Genre {self.genre}>'
-
-    def serialize(self):
-        return {
-            'genre' : self.genre
-        }
 
 
 
 # ------- CHAPTERS -------- #
-mangas_chapters = db.Table('mangas_chapters',
-    db.Column('manga_id', db.Integer, db.ForeignKey('mangas.id'), primary_key=True),
-    db.Column('chapter_id', db.Integer, db.ForeignKey('chapters.id'), primary_key=True)
-)
-
 class Chapters(db.Model):
     __tablename__ = 'chapters'
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     slug = db.Column(db.String(100), nullable=False)
-    chapter_link = db.Column(db.String(300), nullable=False)
-    updated = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
-    manga = db.relationship('Mangas', secondary=mangas_chapters, backref=db.backref('chapters', lazy='dynamic'))
-    history = db.relationship('History', backref='chapters', lazy='dynamic')
+    link = db.Column(db.String(300), nullable=False)
+    manga_id = db.Column(db.Integer, db.ForeignKey('mangas.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.datetime.now(), onupdate=datetime.datetime.now())
 
-    def __init__(self, title, slug, chapter_link, updated):
+    def __init__(self, title, slug, link, manga_id):
         self.title = title
         self.slug = slug
-        self.chapter_link = chapter_link
-        self.updated = updated
+        self.link = link
+        self.manga_id = manga_id
 
     def __repr__(self):
-        return f'<Chapter {self.name}>'
+        return f'<Chapter {self.title}>'
 
     def serialize(self):
         return {
-            'id': self.id,
-            'title': self.title,
-            'slug': self.slug,
-            'chapter_link': self.chapter_link,
-            'updated': self.updated
+            'chapter_id': self.id,
+            'chapter_title': self.title,
+            'chapter_slug': self.slug,
+            'chapter_link': self.link,
+            'chapter_manga_id': self.manga_id,
+            'chapter_created_at': self.created_at,
+            'chapter_updated_at': self.updated_at
         }
 
 if __name__ == '__main__':
-    print(Mangas.query.filter_by(title='Martial Peak').first().chapters.all())
+    ...
+    # print(Mangas.query.filter_by(title='Martial Peak').first().chapters.all())
--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
+# -------------- ALLOW SELF EXECUTE --------------- #
+import os, sys
+sys.path.append(os.getcwd())
+
+
 # ------------------ IMPORTING -------------------- #
 import datetime
 import requests
@@ -7,28 +12,24 @@ from extensions import db
 
 # -------------------- MODELS --------------------- #
 
+# -------------------- USERS --------------------- #
 class Users(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
+    active = db.Column(db.Boolean, default=True)
     username = db.Column(db.String(255), unique=True, nullable=False, default='')
     password = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False)
-    updated_at = db.Column(db.DateTime, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.datetime.now(), onupdate=datetime.datetime.now())
     main_page = db.Column(db.String(255), default='/latest_updates')
-    history = db.relationship('History', backref='user', lazy='dynamic')
-    favorites = db.relationship('Favorites', backref='user', lazy='dynamic')
-    ratings = db.relationship('Ratings', backref='user', lazy='dynamic')
     theme = db.Column(db.String(255), default='light')
 
     def __init__(self, email, password):
         self.email = email
         self.password = password
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
         self.username = self.randomUsername()
-        self.main_page = '/latest_updates'
 
     def randomUsername(self):
         output = [
@@ -43,69 +44,92 @@ class Users(db.Model):
 
     def serialize(self):
         return {
-            'id': self.id,
-            'username': self.username,
-            'password': self.password,
-            'email': self.email,
-            'created_at': self.created_at,
-            'updated_at': self.updated_at,
-            'main_page': self.main_page
+            'user_id': self.id,
+            'user_active': self.active,
+            'user_username': self.username,
+            'user_email': self.email,
+            'user_created_at': self.created_at,
+            'user_updated_at': self.updated_at,
+            'user_main_page': self.main_page,
+            'user_theme': self.theme
         }
 
-    
+
+
+
+# -------------------- HISTORY --------------------- #
+history_fk_chapter = db.Table('history_fk_chapter',
+    db.Column('history_id', db.Integer, db.ForeignKey('history.id')),
+    db.Column('chapter_id', db.Integer, db.ForeignKey('chapters.id'))
+)
+
+
 class History(db.Model):
     __tablename__ = 'history'
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     manga_id = db.Column(db.Integer, db.ForeignKey('mangas.id'), nullable=False)
-    chapter_id = db.Column(db.Integer, db.ForeignKey('chapters.id'))
-    updated_at = db.Column(db.DateTime, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.datetime.now(), onupdate=datetime.datetime.now())
 
-    def __init__(self, user_id, manga_id, chapter_id):
+    # -- relationships -- #
+    # user = db.relationship('Users', backref=db.backref('history', lazy='dynamic'))
+    # manga = db.relationship('Mangas', backref=db.backref('history', lazy='dynamic'))
+    # chapters = db.relationship('Chapters', secondary=history_fk_chapter, backref='history', lazy='dynamic')
+
+    def __init__(self, user_id, manga_id):
         self.user_id = user_id
         self.manga_id = manga_id
-        self.chapter_id = chapter_id
-        self.updated_at = datetime.datetime.now()
 
     def __repr__(self):
         return '<History %r>' % self.id
 
     def serialize(self):
         return {
-            'id': self.id,
-            'user_id': self.user_id,
-            'manga_id': self.manga_id,
-            'chapter_id': self.chapter_id,
-            'updated_at': self.updated_at
+            'history_id': self.id,
+            'history_user_id': self.user_id,
+            'history_manga_id': self.manga_id,
+            'history_created_at': self.created_at,
+            'history_updated_at': self.updated_at
         }
 
 
+
+
+# -------------------- FAVORITES --------------------- #
 class Favorites(db.Model):
     __tablename__ = 'favorites'
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     manga_id = db.Column(db.Integer, db.ForeignKey('mangas.id'), nullable=False)
-    updated_at = db.Column(db.DateTime, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
+
+    # -- relationships -- #
+    # user = db.relationship('Users', backref=db.backref('favorites', lazy='dynamic'))
+    # manga = db.relationship('Mangas', backref=db.backref('favorites', lazy='dynamic'))
 
     def __init__(self, user_id, manga_id):
         self.user_id = user_id
         self.manga_id = manga_id
-        self.updated_at = datetime.datetime.now()
 
     def __repr__(self):
         return '<Favorite %r>' % self.id
 
     def serialize(self):
         return {
-            'id': self.id,
-            'user_id': self.user_id,
-            'manga_id': self.manga_id,
-            'updated_at': self.updated_at
+            'favorites_id': self.id,
+            'favorites_user_id': self.user_id,
+            'favorites_manga_id': self.manga_id,
+            'favorites_updated_at': self.updated_at
         }
 
 
+
+
+
+# -------------------- RATINGS --------------------- #
 class Ratings(db.Model):
     __tablename__ = 'ratings'
 
@@ -113,22 +137,32 @@ class Ratings(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     manga_id = db.Column(db.Integer, db.ForeignKey('mangas.id'), nullable=False)
     rating = db.Column(db.Integer, nullable=False)
-    updated_at = db.Column(db.DateTime, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.datetime.now(), onupdate=datetime.datetime.now())
+
+    # -- relationships -- #
+    # user = db.relationship('Users', backref=db.backref('ratings', lazy='dynamic'))
+    # manga = db.relationship('Mangas', backref=db.backref('ratings', lazy='dynamic'))
 
     def __init__(self, user_id, manga_id, rating):
         self.user_id = user_id
         self.manga_id = manga_id
         self.rating = rating
-        self.updated_at = datetime.datetime.now()
 
     def __repr__(self):
         return '<Rating %r>' % self.id
 
     def serialize(self):
         return {
-            'id': self.id,
-            'user_id': self.user_id,
-            'manga_id': self.manga_id,
-            'rating': self.rating,
-            'updated_at': self.updated_at
+            'ratings_id': self.id,
+            'ratings_user_id': self.user_id,
+            'ratings_manga_id': self.manga_id,
+            'ratings_rating': self.rating,
+            'ratings_created_at': self.created_at,
+            'ratings_updated_at': self.updated_at
         }    
+
+
+
+if __name__ == '__main__':
+    print(Users.query.first().serialize())
